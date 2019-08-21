1881844a703f1333ee3ca2684c52cfb0b668cb02
Andrew Liu <17liut@gmail.com>
2019-08-21 19:36:31 -0400

    Add antialiasing

--- src/image.rs
+++ src/image.rs
@@ -1,5 +1,7 @@
 use image::{ImageBuffer, Rgb};
 
+use crate::vec3::Vec3;
+
 pub struct Image {
     buffer: ImageBuffer<Rgb<u8>, Vec<u8>>,
 }
@@ -7,11 +9,11 @@ pub struct Image {
 impl Image {
     pub fn new<F>(width: u32, height: u32, mut f: F) -> Image
     where
-        F: FnMut(u32, u32) -> [u8; 3],
+        F: FnMut(u32, u32) -> Vec3,
     {
         Image {
             buffer: ImageBuffer::from_fn(width, height, |x, y| {
-                Rgb(f(x, height - y - 1)) // invert y-axis
+                Rgb(f(x, height - y - 1).to_rgb()) // invert y-axis
             }),
         }
     }
--- src/rt.rs
+++ src/rt.rs
@@ -1,5 +1,7 @@
 use std::f64;
 
+use rand::Rng;
+
 use crate::camera::Camera;
 use crate::config::Config;
 use crate::image::Image;
@@ -9,6 +11,7 @@ use crate::vec3::Vec3;
 
 pub fn render(config: Config) -> Result<(), String> {
     let (width, height) = (200, 100);
+    let samples = 100;
 
     let camera = Camera::new(
         Vec3::new(0.0, 0.0, 0.0),
@@ -22,11 +25,16 @@ pub fn render(config: Config) -> Result<(), String> {
     scene.add_object(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0));
 
     let image = Image::new(width, height, |x, y| {
-        let h = x as f64 / width as f64;
-        let v = y as f64 / height as f64;
+        (0..samples)
+            .map(|_| {
+                let h = (x as f64 + rand()) / width as f64;
+                let v = (y as f64 + rand()) / height as f64;
+                let ray = camera.ray(h, v);
 
-        let ray = camera.ray(h, v);
-        color(ray, &scene)
+                color(ray, &scene)
+            })
+            .fold(Vec3::new(0.0, 0.0, 0.0), |sum, x| sum + x)
+            / samples as f64
     });
 
     image.save(config.file)?;
@@ -34,13 +42,17 @@ pub fn render(config: Config) -> Result<(), String> {
     Ok(())
 }
 
-fn color(ray: Ray, scene: &Scene) -> [u8; 3] {
+fn color(ray: Ray, scene: &Scene) -> Vec3 {
     if let Some(hit_data) = scene.hit(ray, 0.0, f64::MAX) {
-        return (0.5 * (hit_data.normal + Vec3::new(1.0, 1.0, 1.0))).to_rgb();
+        return 0.5 * (hit_data.normal + Vec3::new(1.0, 1.0, 1.0));
     }
 
     let unit = ray.direction.normalize();
     let t = 0.5 * (unit.y + 1.0);
-    let col = ((1.0 - t) * Vec3::new(1.0, 1.0, 1.0)) + (t * Vec3::new(0.5, 0.7, 1.0));
-    col.to_rgb()
+
+    ((1.0 - t) * Vec3::new(1.0, 1.0, 1.0)) + (t * Vec3::new(0.5, 0.7, 1.0))
+}
+
+fn rand() -> f64 {
+    rand::thread_rng().gen()
 }
